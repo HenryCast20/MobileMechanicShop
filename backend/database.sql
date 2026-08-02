7d4f6060105e2f7544920ca710e45a56a9a5e52f
HenryCast20 <castellanoshenry20@gmail.com>
2026-08-01 22:27:38 -0400

    Update database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -1,4 +1,4 @@
-Create schema DadShop;
+Create database DadShop;
 use DadShop;
 
 Create table customers(
@@ -6,17 +6,17 @@ customer_id int auto_increment primary key,
 first_name varchar(255) not null,
 last_name varchar(255) not null,
 phone_number varchar(20),
-email varchar(255)
+email varchar(255) unique
 );
 
-create table accounts(
-customer_id int auto_increment primary key,
-username varchar(255) not null,
-password_hash varchar(255) not null,
-created_at Timestamp default current_timestamp,
+Create table accounts(
+customer_id int primary key,
+username varchar(255) not null unique,
+password_hash varchar(255),
+google_id varchar(255) unique,
+created_at timestamp default current_timestamp,
 foreign key (customer_id) references customers(customer_id) on delete cascade
 );
-
 
 Create table vehicles(
 car_id int auto_increment primary key,
@@ -25,16 +25,15 @@ year_produced int,
 make varchar(255),
 model varchar(255),
 odometer int,
-vin varchar (17) unique,
+vin varchar(17) unique,
 foreign key (customer_id) references customers(customer_id) on delete cascade
 );
-
 
 Create table repairs(
 repair_id int auto_increment primary key,
 car_id int not null,
 customer_id int not null,
-transaction_id varchar(255),
+transaction_id varchar(255) unique,
 service_date date,
 odometer_cur int,
 category varchar(255),
@@ -51,10 +50,8 @@ appointment_id int auto_increment primary key,
 car_id int not null,
 customer_id int not null,
 app_date datetime not null,
-status VARCHAR(50) DEFAULT 'Pending', -- Pending, Confirmed, Completed, Cancelled
+status ENUM('Pending', 'Confirmed', 'Completed', 'Cancelled') DEFAULT 'Pending',
 notes text,
 foreign key (car_id) references vehicles(car_id),
 foreign key (customer_id) references customers(customer_id) on delete cascade
 );
-
-
