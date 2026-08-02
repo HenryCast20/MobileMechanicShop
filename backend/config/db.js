90509c90410772311db7b25d9e5db5ab0d7c8307
HenryCast20 <castellanoshenry20@gmail.com>
2026-08-01 20:36:18 -0400

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'DadShop',
   waitForConnections: true,
   connectionLimit: 10,
