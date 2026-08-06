fae3c43ad8cbb420a97d3ce54f4f7e0f40c5a03a
HenryCast20 <castellanoshenry20@gmail.com>
2026-08-06 17:20:51 -0400

    Create vehicleController.js

---
+++ backend/controllers/vehicleController.js
@@ -0,0 +1,35 @@
+const db  = require ('../config/db');
+
+// load the current users logged cars
+const getCars = async (req, res) => {
+    try{
+        const customerId = req.user.customer_id;
+        const [vehicles] = await db.query('SELECT * FROM vehicles WHERE customer_id = ?', [customerId])
+        
+        res.status(200).json(vehicles);
+    }catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error while fetching vehicles.' });
+      }
+    };
+
+
+    //ADD A CAR to logged in user
+    const addCar = async (req, res) => {
+        try {
+            const customerId = req.user.customer_id;
+            const {make, model, year_produced, vin,odometer,license_plate} = req.body;
+
+            const [result] = await db.query(
+            'INSERT INTO vehicles (customer_id, make, model, year, vin, odometer, license_plate) VALUES (?, ?, ?, ?, ?)', [customerId, make, model, year_produced, vin,odometer,license_plate]
+            );
+
+            res.status(201).json({ message: 'Vehicle added successfully!', car_id: result.insertId });
+        } catch (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Server error while adding vehicle.' });
+        }
+      };
+
+
+module.exports = { getVehicles, addVehicle };
