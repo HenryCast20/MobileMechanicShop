57adbe66ad60b1697c1cabde2c0c9066aea6a3ba
HenryCast20 <castellanoshenry20@gmail.com>
2026-08-06 18:48:36 -0400

    Update vehicleController.js

--- backend/controllers/vehicleController.js
+++ backend/controllers/vehicleController.js
@@ -14,22 +14,22 @@ const getCars = async (req, res) => {
     };
 
 
-    //ADD A CAR to logged in user
-    const addCar = async (req, res) => {
-        try {
+          const addCar = async (req, res) => {
+          try {
             const customerId = req.user.customer_id;
-            const {make, model, year_produced, vin,odometer,license_plate} = req.body;
-
+            const { make, model, year, vin, odometer, license_plate } = req.body;
+        
             const [result] = await db.query(
-            'INSERT INTO vehicles (customer_id, make, model, year, vin, odometer, license_plate) VALUES (?, ?, ?, ?, ?)', [customerId, make, model, year_produced, vin,odometer,license_plate]
+              'INSERT INTO vehicles (customer_id, make, model, year, vin, odometer, license_plate) VALUES (?, ?, ?, ?, ?, ?, ?)',
+              [customerId, make, model, year, vin, odometer, license_plate]
             );
-
+        
             res.status(201).json({ message: 'Vehicle added successfully!', car_id: result.insertId });
-        } catch (err) {
-          console.error(err);
-          res.status(500).json({ error: 'Server error while adding vehicle.' });
-        }
-      };
+          } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Server error while adding vehicle.' });
+          }
+        };
 
 
 module.exports = { getCars, addCar };
