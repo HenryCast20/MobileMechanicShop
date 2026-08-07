cb13491f634c56a03e90b026f94a7666c0157cb7
HenryCast20 <castellanoshenry20@gmail.com>
2026-08-07 17:15:35 -0400

    Update app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
 import MyVehicles from "./pages/mycars";
+import CustomerInvoices from "./pages/CustomerInvoices";
 
 export default function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
@@ -14,6 +15,9 @@ export default function App() {
   // Switch between views based on state
   if (currentView === "myVehicles") {
     return <MyVehicles user={user} setUser={setUser} setCurrentView={setCurrentView} />;
+  }
+  if (currentView === "myInvoices") {
+    return <CustomerInvoices user={user} setUser={setUser} setCurrentView={setCurrentView} />;
   }
 
   return <Dashboard user={user} setUser={setUser} setCurrentView={setCurrentView} />;
