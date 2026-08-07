4325252729412f6adf416f99818a153f7ea6c6f2
HenryCast20 <castellanoshenry20@gmail.com>
2026-08-07 17:16:24 -0400

    Update dashboard.jsx

--- client/pages/dashboard.jsx
+++ client/pages/dashboard.jsx
@@ -110,7 +110,7 @@ export default function Dashboard({ user, setUser ,setCurrentView}){
           <div className="dashboard-card">
             <h3>🧾 Invoices</h3>
             <p>View invoices, payments, and receipts.</p>
-            <button>View Invoices</button>
+          <button onClick={() => setCurrentView("myInvoices")}>View Invoices</button>
           </div>
 
         </section>
