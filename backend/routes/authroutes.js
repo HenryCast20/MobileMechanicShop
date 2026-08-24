c989f723e5f07e0aa70b8ff17b9a232b98e27dce
Henry Castellanos <castellanoshenry20@gmail.com>
2026-08-23 20:28:22 -0400

    Update authroutes.js

--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register, loginUser, googleLogin } = require('../controllers/authcontroller');
+const { register, loginUser, googleLogin,logout  } = require('../controllers/authcontroller');
 
 router.post('/register', register);
 router.post('/login', loginUser);
 router.post('/google', googleLogin);
+router.post('/logout', logout);
 
 module.exports = router;
