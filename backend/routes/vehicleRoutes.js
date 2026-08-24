e2f1726885d270719cb99e2f2a8960c25263f0c0
Henry Castellanos <castellanoshenry20@gmail.com>
2026-08-23 22:51:37 -0400

    Update vehicleRoutes.js

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -1,10 +1,11 @@
 const express = require ('express');
 const router = express.Router();
-const {getCars, addCar} = require('../controllers/vehicleController');
+const {getCars, addCar, updateCar} = require('../controllers/vehicleController');
 const verifyToken = require('../middleware/auth'); 
 
 router.get('/', verifyToken, getCars); // get all the cars from the user logged in 
 
 router.post('/',verifyToken, addCar); // add a car to the list 
+router.put('/:id', verifyToken, updateCar);
 
 module.exports = router;
